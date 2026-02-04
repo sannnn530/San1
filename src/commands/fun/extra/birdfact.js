507844f1f0f5f0c81ee75e8fe45e885af9902c89
sannnn530 <sannnn530@gmail.com>
2026-02-04 07:03:40 +0700

    Add bird fact command to fetch random bird facts

---
+++ src/commands/fun/extra/birdfact.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const fetch = require("node-fetch");
+
+module.exports = async (client, interaction, args) => {
+
+    fetch(
+        `https://some-random-api.ml/facts/bird`
+    )
+        .then((res) => res.json()).catch({})
+        .then(async (json) => {
+            client.embed({
+                title: `💡・Random bird fact`,
+                desc: json.fact,
+                type: 'editreply',
+            }, interaction);
+        }).catch({})
+}
